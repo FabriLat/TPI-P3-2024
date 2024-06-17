6375e1f706804bb77e58aac67f077625c83fae3b
JuaniTorti <nachi.tortjuani@gmail.com>
2024-06-17 12:30:51 -0300

    Creacion de Repositoris
    Los metodos definidos dentro son a modo de ejemplo para conocer la utilizacion del contexto

---
+++ src/Infrastructure/Data/Repositories/MovieRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class MovieRepository
+    {
+        private readonly ApplicationContext _context;
+        public MovieRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public List<Movie> GetAll(int id)
+        {
+            return _context.Movies.ToList();
+        }
+    }
+}
---
+++ src/Infrastructure/Data/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class UserRepository
+    {
+        private readonly ApplicationContext _context;
+        public UserRepository(ApplicationContext context) 
+        { 
+            _context = context;
+        }
+
+        public User? GetUser(int id)
+        {
+            return _context.Users.FirstOrDefault(user => user.Id == id);
+        }
+    }
+}
