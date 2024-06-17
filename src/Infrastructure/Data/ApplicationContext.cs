84e9611cb9637cf4de691e6a8d2c3acb875fa2db
JuaniTorti <nachi.tortjuani@gmail.com>
2024-06-17 12:24:43 -0300

    Creacion de Context
    Luego de definir los repositorios que seran utilizados, se debe intentar hacer la migracion inicial

---
+++ src/Infrastructure/Data/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        private readonly bool isTestingEnvironment;
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Show> Shows { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options, bool isTestingEnvironment = false) : base(options) 
+        {
+            this.isTestingEnvironment = isTestingEnvironment;
+        }
+    }
+}
