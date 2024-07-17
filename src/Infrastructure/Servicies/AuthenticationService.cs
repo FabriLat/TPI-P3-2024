e2c3b958ad9bded38f876b69e1c0f8889ff8faf3
JuaniTorti <nachi.tortjuani@gmail.com>
2024-07-17 00:23:25 -0300

    creacion de archivos e inicio de autenticacion
    Falta instalar los paquetes necesarios

---
+++ src/Infrastructure/Servicies/AuthenticationService.cs
@@ -0,0 +1,92 @@
+﻿using Application.Interfaces;
+using Application.Models;
+using Domain.Entities;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Infrastructure.Servicies
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly AutenticacionServiceOptions _options;
+
+        public AuthenticationService(IUserRepository userRepository, IOptions<AutenticacionServiceOptions> options)
+        {
+            _userRepository = userRepository;
+            _options = options.Value;
+        }
+
+        private User? ValidateUser(AuthenticationRequest authenticationRequest)
+        {
+            if (string.IsNullOrEmpty(authenticationRequest.UserName) || string.IsNullOrEmpty(authenticationRequest.Password))
+                return null;
+
+            var user = _userRepository.GetUserByName(authenticationRequest.UserName);
+
+            if (user == null) 
+                return null;
+
+
+            if (user.Password == authenticationRequest.Password) 
+                return user;
+
+
+            return null;
+
+        }
+
+
+        public string Autenticar(AuthenticationRequest authenticationRequest)
+        {
+            //Paso 1: Validamos las credenciales
+            var user = ValidateUser(authenticationRequest); //Lo primero que hacemos es llamar a una función que valide los parámetros que enviamos.
+
+            if (user == null)
+            {
+                return("User authentication failed");
+            }
+
+
+            //Paso 2: Crear el token
+            var securityPassword = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_options.SecretForKey)); //Traemos la SecretKey del Json. agregar antes: using Microsoft.IdentityModel.Tokens;
+
+            var credentials = new SigningCredentials(securityPassword, SecurityAlgorithms.HmacSha256);
+
+            //Los claims son datos en clave->valor que nos permite guardar data del usuario.
+            var claimsForToken = new List<Claim>();
+            claimsForToken.Add(new Claim("sub", user.Id.ToString())); 
+            claimsForToken.Add(new Claim("given_name", user.UserName)); 
+            claimsForToken.Add(new Claim("role",user.UserRole.ToString())); 
+
+            var jwtSecurityToken = new JwtSecurityToken( //agregar using System.IdentityModel.Tokens.Jwt; Acá es donde se crea el token con toda la data que le pasamos antes.
+              _options.Issuer,
+              _options.Audience,
+              claimsForToken,
+              DateTime.UtcNow,
+              DateTime.UtcNow.AddHours(1),
+              credentials);
+
+            var tokenToReturn = new JwtSecurityTokenHandler() //Pasamos el token a string
+                .WriteToken(jwtSecurityToken);
+
+            return tokenToReturn.ToString();
+        }
+
+
+        public class AutenticacionServiceOptions
+        {
+            public const string AutenticacionService = "AutenticacionService";
+
+            public string Issuer { get; set; }
+            public string Audience { get; set; }
+            public string SecretForKey { get; set; }
+        }
+    }
+}
---
+++ src/WebCinema/Controllers/AuthenticationController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebCinema.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        public IActionResult Authenticate([FromBody] AuthenticationRequest credentials)
+        {
+            string token = "";
+
+            return Ok(token);
+        }
+    }
+}
